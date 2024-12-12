7f3a505530882cda79efe8f2db7d693a55b4692f
Parth Antala <theparthantala@gmail.com>
2024-12-12 07:00:38 -0500

    Initial Commit

---
+++ Stickers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stickers
+//
+//  Created by Parth Antala on 2024-12-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by Parth Antala on 2024-12-12.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
